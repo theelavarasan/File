a2499757fbd717f7b18478a8341f4333063fafbb
vtg users <elavarasansiva123@gmail.com>
2022-09-29 13:45:40 +0530

    Initial commit

---
+++ src/main/java/com/Upload/File/Controller/FileUploadController.java
@@ -0,0 +1,35 @@
+package com.Upload.File.Controller;
+
+import com.Upload.File.FileUploadResponse;
+import com.Upload.File.FileUploadUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+public class FileUploadController {
+    @PostMapping("/uploadFile")
+    public ResponseEntity<FileUploadResponse> uploadFile(
+            @RequestParam("file") MultipartFile multipartFile)
+            throws IOException {
+
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        long size = multipartFile.getSize();
+
+        String filecode = FileUploadUtil.saveFile(fileName, multipartFile);
+
+        FileUploadResponse response = new FileUploadResponse();
+        response.setFileName(fileName);
+        response.setSize(size);
+        response.setDownloadUri("/downloadFile/" + filecode);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+}
